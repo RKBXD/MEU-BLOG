13e3930985742cd6fa294bad9723c7766f4ea09a
Erik Pereira Rodrigues <rodrigues.erik.jr@gmail.com>
2022-05-16 22:00:14 -0300

    Blog1.0

---
+++ blogjs.js
@@ -0,0 +1,21 @@
+var insta = document.getElementById('insta')
+var canal = document.getElementById('canal')
+var git = document.getElementById('git')
+var projetos = document.getElementById('projetos')
+var zap = document.getElementById('zap')
+var mail = document.getElementById('mail')
+
+function enter(){insta.style.backgroundImage='linear-gradient(45deg,rgb(132, 0, 255),rgb(55, 0, 255),rgb(165, 165, 165),rgb(132, 0, 255))'}
+function saiu() {insta.style.background='blueviolet'}
+function enterr() {canal.style.backgroundImage='linear-gradient(45deg,rgb(132, 0, 255),rgb(55, 0, 255),rgb(165, 165, 165),rgb(132, 0, 255))'}
+function saiuu() {canal.style.background='blueviolet'}
+function enterrr() {git.style.backgroundImage='linear-gradient(45deg,rgb(132, 0, 255),rgb(55, 0, 255),rgb(165, 165, 165),rgb(132, 0, 255))'}
+function saiuuu() {git.style.background='blueviolet'}
+function enterrrr() {projetos.style.backgroundImage='linear-gradient(45deg,rgb(132, 0, 255),rgb(55, 0, 255),rgb(165, 165, 165),rgb(132, 0, 255))'}
+function saiuuuu() {projetos.style.background='blueviolet'}
+function zapenter() {zap.style.background='blueviolet'}
+function zapsaiu() {zap.style.background='black'}
+function mailenter() {mail.style.background='blueviolet'}
+function mailsaiu() {mail.style.background='black'}
+
+window.alert('ESTE SITE AINDA NÃO ESTA PRONTO 100%!!!')
